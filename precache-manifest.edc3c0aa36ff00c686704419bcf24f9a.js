91562add01408ca28c6a7136d65cc9daec8c5ad1
burvxs <benny.purvis@hotmail.com>
2020-10-12 19:25:58 +1100

    Updates

---
+++ precache-manifest.edc3c0aa36ff00c686704419bcf24f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7a557ba6ca1f04d6ae1dc3cb2d2bf2",
+    "url": "/journal-react/index.html"
+  },
+  {
+    "revision": "b3d8ad3724a8269c0484",
+    "url": "/journal-react/static/css/main.f0d03181.chunk.css"
+  },
+  {
+    "revision": "938e7385fbfeddd4d47a",
+    "url": "/journal-react/static/js/2.c13284dd.chunk.js"
+  },
+  {
+    "revision": "ec1add61b541a6106f0424d88e93f613",
+    "url": "/journal-react/static/js/2.c13284dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d8ad3724a8269c0484",
+    "url": "/journal-react/static/js/main.d3127cf1.chunk.js"
+  },
+  {
+    "revision": "7a253c27904b9a7e57db",
+    "url": "/journal-react/static/js/runtime-main.3d04efdd.js"
+  }
+]);
